16b3f25fb5eae58113ba3a1e87dc854f629f7a4a
Bruno_tiaro <brunoaudi2003@gmail.com>
2025-10-18 02:43:09 -0300

    Aumentei playerscript e enemyscript

--- GJ+25-3D/Assets/Scripts/EnemyScript.cs
+++ GJ+25-3D/Assets/Scripts/EnemyScript.cs
@@ -4,16 +4,23 @@ public class EnemyScript : MonoBehaviour
 {
     public int health;
     public float speed;
-
+    private PlayerScript player;
 
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x > player.transform.position.x)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if(transform.position.x < player.transform.position.x)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 }
