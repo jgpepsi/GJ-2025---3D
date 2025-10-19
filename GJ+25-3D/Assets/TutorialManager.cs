7538ddb8c0cca714ba252893a16d54e690ca6191
gustavodillv <gustavodillv@gmail.com>
2025-10-19 15:48:38 -0300

    Tutorial new com fade na régua

--- GJ+25-3D/Assets/TutorialManager.cs
+++ GJ+25-3D/Assets/TutorialManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
     public float timeToStartFade;
     public float fadeDuration;
+    public Image button1;
+    public Image button2;
+    Color newColor;
 
     private void Start()
     {
-        
+        StartCoroutine(FadeCountdown());
     }
 
     private IEnumerator FadeCountdown()
     {
+        yield return new WaitForSeconds(timeToStartFade);
+        StartCoroutine(Fade());
+    }
 
+    private IEnumerator Fade()
+    {
+        newColor = button1.color;
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / fadeDuration);
+            newColor.a = Mathf.Lerp(1, 0, t);
+            button1.color = newColor;
+            button2.color = newColor;
+            yield return null;
+        }
     }
 }
